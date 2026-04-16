5dbc725ec12dabc0d5de250c87845a6b123fe9d6
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-04-16 18:50:05 +0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  service: String,
+  userId: String,
+  serviceId: String,
   link: String,
   quantity: Number,
   providerOrderId: String,
-  status: { type: String, default: "pending" }
+  status: {
+    type: String,
+    default: "pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Order", orderSchema);
