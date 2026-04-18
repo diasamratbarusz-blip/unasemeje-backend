b5a7853af39979aee23d4391909de56ccc056163
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-04-18 22:48:09 +0300

    Update Deposit.js

--- models/Deposit.js
+++ models/Deposit.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
 const depositSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  phone: { type: String, required: true },
-  amount: { type: Number, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 
-  code: { type: String, unique: true, required: true },
+  phone: String,
+  amount: Number,
+
+  transactionCode: {
+    type: String,
+    unique: true,
+    required: true
+  },
+
+  message: String, // FULL M-PESA MESSAGE pasted by user
 
   status: {
     type: String,
-    enum: ["pending", "approved", "rejected"],
     default: "pending"
-  },
+  }
 
-  flagged: { type: Boolean, default: false },
-
-  createdAt: { type: Date, default: Date.now }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Deposit", depositSchema);
