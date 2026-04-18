1851b64cdae47eef03c773b51c60b8c9824730be
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-04-18 22:37:00 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const UserSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      unique: true,
       required: true,
+      unique: true,
       lowercase: true,
       trim: true
     },
